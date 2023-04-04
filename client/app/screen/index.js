68f0ffd2cab20e10bf752a04d2ed6968ef8f7e56
Na Lin <lna483018@gmail.com>
2023-04-03 22:12:16 -0400

    create index for screen folder

---
+++ client/app/screen/index.js
@@ -0,0 +1,56 @@
+// Landing
+import HomeScreen from "./HomeScreen";
+
+// login
+import LogInScreen from "./LogInScreen";
+
+// sign up
+import SignUpScreen from "./SignUpScreen";
+
+// Home
+import DashHome from "./DashHome";
+
+// FAQ
+import FAQ from "./FAQ";
+
+// Course
+import CourseScreen from "./CourseScreen";
+import CourseStudentScreen from "./CourseStudentScreen";
+import CourseAssessmentsScreen from "./CourseAssessmentsScreen";
+
+// Assessment
+import AssessmentsScreen from "./AssessmentsScreen";
+import CreateAssessmentScreen from "./CreateAssessmentScreen";
+import EditAssessmentScreen from "./EditAssessmentScreen";
+import GradingScreen from "./GradingScreen";
+
+// Report
+import StudentReportScreen from "./StudentReportScreen";
+import AssessmentReportScreen from "./AssessmentReportScreen";
+import CourseReportScreen from "./CourseReportScreen";
+
+// Not found
+import NotFound from "./NotFound";
+
+// Student view
+import StudentViewScreen from "./StudentViewScreen";
+
+export {
+  HomeScreen,
+  LogInScreen,
+  SignUpScreen,
+  DashHome,
+  FAQ,
+  CourseScreen,
+  CourseStudentScreen,
+  CourseAssessmentsScreen,
+  AssessmentsScreen,
+  CreateAssessmentScreen,
+  EditAssessmentScreen,
+  GradingScreen,
+  StudentReportScreen,
+  AssessmentReportScreen,
+  CourseReportScreen,
+  NotFound,
+  StudentViewScreen,
+};
